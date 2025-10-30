fbee4c5ea928af863dd3a4f7d2790e454c079cf6
Chan Myae <cm27102003@gmail.com>
2025-10-30 16:14:58 +0700

    Change to accommodate task 3a

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -70,6 +70,66 @@ public class App
         }
     }
 
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT e.emp_no, e.first_name, e.last_name, s.salary, t.title, d.dept_name, dm.emp_no AS manager_emp_no, m.first_name AS manager_first_name, m.last_name AS manager_last_name " +
+                    "FROM employees e " +
+                    "JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01' " +
+                    "JOIN titles t ON e.emp_no = t.emp_no AND t.to_date = '9999-01-01' " +
+                    "JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01' " +
+                    "JOIN departments d ON de.dept_no = d.dept_no " +
+                    "JOIN dept_manager dm ON d.dept_no = dm.dept_no AND dm.to_date = '9999-01-01' " +
+                    "JOIN employees m ON dm.emp_no = m.emp_no " +
+                    "WHERE e.emp_no = " + ID  ;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                emp.salary = rset.getInt("salary");
+                emp.title = rset.getString("title");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager_first_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+
     /**
      * Gets all the current employees and salaries.
      * @return A list of all employees and salaries, or null if there is an error.
@@ -135,86 +195,23 @@ public class App
         // Connect to database
         a.connect();
 
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+
+
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
         // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+//        System.out.println(employees.size());
 
         a.printSalaries(employees);
-
-//        // Get Employee
-//        Employee emp = a.getEmployee(255530);
-//        // Display results
-//        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
     }
 }
 
-//    public Employee getEmployee(int ID)
-//    {
-//        try
-//        {
-//            Statement stmt = con.createStatement();
-//            String strSelect =
-//                    "SELECT e.emp_no, e.first_name, e.last_name, " +
-//                            "t.title, s.salary, d.dept_name, " +
-//                            "CONCAT(m.first_name, ' ', m.last_name) AS manager " +
-//                            "FROM employees e " +
-//                            "JOIN titles t ON e.emp_no = t.emp_no " +
-//                            "JOIN salaries s ON e.emp_no = s.emp_no " +
-//                            "JOIN dept_emp de ON e.emp_no = de.emp_no " +
-//                            "JOIN departments d ON de.dept_no = d.dept_no " +
-//                            "JOIN dept_manager dm ON d.dept_no = dm.dept_no " +
-//                            "JOIN employees m ON dm.emp_no = m.emp_no " +
-//                            "WHERE e.emp_no = " + ID + " " +
-//                            "AND t.to_date = '9999-01-01' " +
-//                            "AND s.to_date = '9999-01-01' " +
-//                            "AND de.to_date = '9999-01-01' " +
-//                            "AND dm.to_date = '9999-01-01';";
-//
-//            ResultSet rset = stmt.executeQuery(strSelect);
-//
-//            if (rset.next())
-//            {
-//                Employee emp = new Employee();
-//                emp.emp_no = rset.getInt("emp_no");
-//                emp.first_name = rset.getString("first_name");
-//                emp.last_name = rset.getString("last_name");
-//                emp.title = rset.getString("title");
-//                emp.salary = rset.getInt("salary");
-//                emp.dept_name = rset.getString("dept_name");
-//                emp.manager = rset.getString("manager");
-//                return emp;
-//            }
-//            else
-//                return null;
-//        }
-//        catch (Exception e)
-//        {
-//            System.out.println(e.getMessage());
-//            System.out.println("Failed to get employee details");
-//            return null;
-//        }
-//    }
-//
-//    public void displayEmployee(Employee emp)
-//    {
-//        if (emp != null)
-//        {
-//            System.out.println("========================================");
-//            System.out.println("Employee ID: " + emp.emp_no);
-//            System.out.println("Name       : " + emp.first_name + " " + emp.last_name);
-//            System.out.println("Title      : " + emp.title);
-//            System.out.println("Department : " + emp.dept_name);
-//            System.out.println("Manager    : " + emp.manager);
-//            System.out.println("Salary     : " + emp.salary);
-//            System.out.println("========================================");
-//        }
-//        else
-//        {
-//            System.out.println("No employee found with that ID.");
-//        }
-//    }
+
