719aaf8b1e1d6496842cebc64f14e3380cfd3599
Chan Myae <cm27102003@gmail.com>
2025-11-22 15:18:06 +0700

    Integration test

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppIntegrationTest
 {
